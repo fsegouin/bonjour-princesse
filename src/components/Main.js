a048a04e5b94a6ae62aa21b980f8e6159ec2680b
fsegouin <florent.segouin@gmail.com>
2017-01-17 16:19:02 +0000

    Update quotes with special days

--- src/components/Main.js
+++ src/components/Main.js
@@ -38,13 +38,17 @@ export default class AppComponent extends React.Component {
 
     if (hash === 'UF6geY+vsYzJQH8TZTt2i+ve660ppmcEdbczPY85jqY=') {
       this.setState({showPage: false});
-      passwordInput.blur();
+        passwordInput.blur();
     }
   }
 
   render() {
-    const day = new Date().getDate();
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth();
+    const duration = Math.ceil((date.getTime() - new Date(2015, 9, 6).getTime()) / (1000*60*60*24));
     const quoteToPick = Math.floor(this.map_range(day, 1, 31, 0, quotesList.quotes.length-1));
+    const isSpecialDay = ((day === 18 && month === 0) || day === 6 ? true : false);
 
     const startClass = cx(
       ['start'],
@@ -67,46 +71,50 @@ export default class AppComponent extends React.Component {
     );
 
     const specialDay = cx(
-      ['special'],
-      { 'hide': day !== 6 }
+        ['special'],
+        {'special--anniversary': day === 6},
+        {'special--birthday': day === 18 && month === 0},
+        { 'hide': !isSpecialDay }
     );
 
-    return (
-      <section>
-        <div className={specialDay}>
-          <div className="ribbon">Special Day</div>
-        </div>
-        <div className={passwordClass}>
-          <h1>What's the magic word?</h1>
-          <input type="password" name="psw" autoFocus onChange={this.checkPassword.bind(this)}
-            ref="passwordInput"/>
-        </div>
-        <div className={startClass}>
-          <div className="castle">
-            <img className="clouds-bg-img" src={cloudsBgImage} />
-            <img className="castle-grass-img" src={castleGrassImage} />
-            <img className="castle-img" src={castleImage} />
-            <img className="castle-shadow-img" src={castleShadowImage} />
-            <img className="clouds-fg-img hidden" src={cloudsFgImage} />
-            <img className="character-princess" src={princessImage} />
-            <div className="character-princess--shadow"></div>
-            <img className="character-prince" src={princeImage} />
-            <div className="character-prince--shadow"></div>
-            <img className="character-cav" src={cavImage} />
-            <div className="character-cav--shadow"></div>
-            <img className="character-pipito" src={pipitoImage} />
-            <div className="character-pipito--shadow"></div>
-          </div>
-          <div className={titleClass}>
-            <h1>Bonjour Princesse,</h1>
-          </div>
-          <div className="quote">
-            <div className={quoteClass}>
-              {quotesList.quotes[quoteToPick].text}
+      return (
+          <section>
+            <div className={specialDay}>
+              <div className="ribbon">{day % 18 ? 'Anniversary' : 'Princess\' Birthday' }</div>
             </div>
-          </div>
-        </div>
-      </section>
+            <div className={passwordClass}>
+              <h1>What's the magic word?</h1>
+              <input type="password" name="psw" autoFocus onChange={this.checkPassword.bind(this)}
+                     ref="passwordInput"/>
+            </div>
+            <div className={startClass}>
+              <div className="castle">
+                <img className="clouds-bg-img" src={cloudsBgImage} />
+                <img className="castle-grass-img" src={castleGrassImage} />
+                <img className="castle-img" src={castleImage} />
+                <img className="castle-shadow-img" src={castleShadowImage} />
+                <img className="clouds-fg-img hidden" src={cloudsFgImage} />
+                <img className="character-princess" src={princessImage} />
+                <div className="character-princess--shadow"></div>
+                <img className="character-prince" src={princeImage} />
+                <div className="character-prince--shadow"></div>
+                <img className="character-cav" src={cavImage} />
+                <div className="character-cav--shadow"></div>
+                <img className="character-pipito" src={pipitoImage} />
+                <div className="character-pipito--shadow"></div>
+              </div>
+              <div className={titleClass}>
+                <h1>Bonjour Princesse,</h1>
+              </div>
+              <div className="quote">
+                <div className={quoteClass}>
+                  {!isSpecialDay ? quotesList.quotes[quoteToPick].text
+                   : (day % 18) ? quotesList.specials[1].text.replace(/\?/i, duration)
+                  : quotesList.specials[0].text}
+                </div>
+              </div>
+            </div>
+          </section>
     );
   }
 }
